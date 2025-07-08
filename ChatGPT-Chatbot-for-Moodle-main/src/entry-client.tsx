ff50b24f2ff311c43ede8a193f0c03b44c7bff8c
michlbetz <mchlbtz@gmail.com>
2025-07-08 11:26:53 -0500

    Delete ChatGPT-Chatbot-for-Moodle-main directory

--- ChatGPT-Chatbot-for-Moodle-main/src/entry-client.tsx
+++
@@ -1,5 +0,0 @@
-import { clientBootstrap, RouteModule } from "@impalajs/react/client";
-
-const modules = import.meta.glob<RouteModule>("./routes/**/*.{tsx,jsx}");
-
-clientBootstrap(modules);
--- ChatGPT-Chatbot-for-Moodle-main/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import impala from "@impalajs/core/plugin";
-
-export default defineConfig({
-  plugins: [react(), impala()],
-});
